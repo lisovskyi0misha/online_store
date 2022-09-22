4a91096902990e592a613d130f5d24bceb5da86b
misha <milisov4@gmal.com>
2022-09-22 10:38:59 +0300

    created admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'landing_page#new'
   resources :landing_page, only: :create
